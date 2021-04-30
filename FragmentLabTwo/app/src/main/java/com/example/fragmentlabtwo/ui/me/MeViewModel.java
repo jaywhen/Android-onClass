57591903826e8a77781fa9cec19925ee40ed0ed4
jaywhen <jaywhen@qq.com>
2021-04-30 12:04:54 +0800

    add fragment

---
+++ FragmentLabTwo/app/src/main/java/com/example/fragmentlabtwo/ui/me/MeViewModel.java
@@ -0,0 +1,18 @@
+package com.example.fragmentlabtwo.ui.me;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MeViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public MeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Me profile");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
